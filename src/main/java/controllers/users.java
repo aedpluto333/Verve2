ba284f64e5416a608864f6b8faf3f080849e6f4d
93317 <93317@farnborough.ac.uk>
2020-10-08 14:50:35 +0100

    Add a list users api method to test it function (code copied from slideshow)

---
+++ src/main/java/controllers/users.java
@@ -0,0 +1,30 @@
+package controllers;
+
+@Path("users/")
+@Consumes(MediaType.MULTIPART_FORM_DATA)
+@Produces(MediaType.APPLICATION_JSON)
+
+
+public class users {
+    @GET
+    @Path("list")
+    public String UsersList() {
+        System.out.println("Invoked Users.UsersList()");
+        JSONArray response = new JSONArray();
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT UserID, UserName FROM Users");
+            ResultSet results = ps.executeQuery();
+            while (results.next()==true) {
+                JSONObject row = new JSONObject();
+                row.put("UserID", results.getInt(1));
+                row.put("UserName", results.getString(2));
+                response.add(row);
+            }
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
+        }
+    }
+
+}
