fd75f12755114d25fb39f4c1f60d936c6afc9b66
93317 <93317@farnborough.ac.uk>
2020-11-02 10:26:13 +0000

    Modifications to message digest system

--- src/controllers/users.java
+++ src/controllers/users.java
@@ -8,26 +8,34 @@ import server.main;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 @Path("users/")
 
 public class users {
      // create function for sha256 hash
     // https://www.baeldung.com/sha-256-hashing-java
-    private static String bytesToHex(byte[] hash) {
-        StringBuffer hexString = new StringBuffer();
-        for (int i = 0; i < hash.length; i++) {
-            String hex = Integer.toHexString(0xff & hash[i]);
-            if (hex.length() == 1) {
-                hexString.append('0');
-            }
-            hexString.append(hex);
-        }
-        return hexString.toString();
-    }
+    // https://docs.google.com/presentation/d/1kpT9LC_3Ckwsk7YjoMKgYHTr9a_DcP3kZQ-oeea9anA/edit?usp=sharing
+       public static void main(String[] args) {
+           String text = "Hello there everybody";
+           System.out.println(text + " --> " + generateHash(text));
+       }
+
+       public static String generateHash(String text) {
+           try {
+               MessageDigest hasher = MessageDigest.getInstance("SHA-256");
+               hasher.update(text.getBytes());
+               return DatatypeConverter.printHexBinary(hasher.digest()).toUpperCase();
+           } catch (NoSuchAlgorithmException nsae) {
+               return nsae.getMessage();
+           }
+       }
 
     @GET
     @Path("loggedin/{UserID}")
@@ -60,18 +68,20 @@ public class users {
     @Produces(MediaType.APPLICATION_JSON)
     public String UserAttemptLogin(@FormDataParam("Username") String Username, @FormDataParam("Password") String Password) {
         System.out.println("Invoked Users.AttemptLogin()");
+        System.out.println("Invoked Users.AttemptLogin()");
         try {
             PreparedStatement ps = main.db.prepareStatement("SELECT Password FROM Users WHERE Username = ?");
             ps.setString(1, Username);
             ResultSet results = ps.executeQuery();
             JSONObject response = new JSONObject();
+
             if (results.next() == true) {
                 MessageDigest digest = MessageDigest.getInstance("SHA-256");
                 byte[] encodehash = digest.digest(Password.getBytes(StandardCharsets.UTF_8));
                 digest = MessageDigest.getInstance("SHA-256");
                 final byte[] hashbytes = digest.digest(Password.getBytes(StandardCharsets.UTF_8));
                 String sha2Hex = bytesToHex(hashbytes);
-                //System.out.println(sha2Hex);
+                System.out.println(sha2Hex);
 
                 // See if password guess equals the password
                 if (results.getString(1) == sha2Hex) {
@@ -80,6 +90,7 @@ public class users {
                     response.put("Success", false);
                 }
             }
+
             return response.toString();
         } catch (Exception exception) {
             System.out.println("Database error: " + exception.getMessage());
