b51d38d4aa3c7927b256022d946f1fb8c9beea6d
93317 <93317@farnborough.ac.uk>
2020-11-02 12:22:22 +0000

    minor modifications

--- src/server/main.java
+++ src/server/main.java
@@ -19,7 +19,7 @@ public class main {
 
     public static void main(String[] args) {
 
-        openDatabase("verve (3).db");                                //connect to our database file, when you stop the server the connection to the database
+        openDatabase("verve.db");                                //connect to our database file, when you stop the server the connection to the database
                                                                             // is closed and you can access it through SQLite Studio
 
         ResourceConfig config = new ResourceConfig();                       // prepare our Jersey Servlet, 'Servlet' is a Java program that runs on a Java-enabled web servers.
