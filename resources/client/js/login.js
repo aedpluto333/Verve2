37f72447a6b2ece1812f31ffb54da4138c2ced4f
aedpluto <amyedp123@outlook.com>
2020-11-26 21:47:59 +0000

    updating login functionality

--- resources/client/js/login.js
+++ resources/client/js/login.js
@@ -1,8 +1,8 @@
-//addUser function to add a user to the database
-function addUser() {
-    console.log("Invoked AddUser()");
-    const formData = new FormData(document.getElementById('InputUserDetails'));
-    let url = "/users/add";
+//login function to add a user to the database
+function logInUser() {
+    console.log("Invoked Login()");
+    const formData = new FormData(document.getElementById('LoginForm'));
+    let url = "/users/login";
     fetch(url, {
         method: "POST",
         body: formData,
@@ -12,14 +12,7 @@ function addUser() {
         if (response.hasOwnProperty("Error")) {
             alert(JSON.stringify(response));
         } else {
-            window.open("/client/welcome.html", "_self");   //URL replaces the current page.  Create a new html file
+            window.open("/client/user.html", "_self");   //URL replaces the current page.  Create a new html file
         }                                                  //in the client folder called welcome.html
     });
 }
-
-//Add the form elements to index.html inside <body> tags
-<form id="InputUserDetails">
-    Username: <input type="text" name="Username">
-    Password: <input type="text" name="Password">
-</form>
-<button onclick='addUser();'>Add User</button>
