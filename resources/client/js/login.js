39c63068302b13681edd98822d26aeb91d94a3e0
aedpluto <amyedp123@outlook.com>
2020-12-09 20:50:40 +0000

    console.logs added for testing

--- resources/client/js/login.js
+++ resources/client/js/login.js
@@ -1,18 +1,22 @@
 // login function to allow user to access the website
 function logInUser() {
-    console.log("Invoked Login()");
+    console.log("Invoked logInUser()");
     const formData = new FormData(document.getElementById('LoginForm'));
     let url = "/users/attemptlogin";
     fetch(url, {
         method: "POST",
         body: formData,
     }).then(response => {
+        console.log("Line 10");
         return response.json()
     }).then(response => {
+        console.log("Line 13");
         if (response.hasOwnProperty("Error")) {
+            console.log("Line 15");
             alert(JSON.stringify(response));
         } else {
             // URL replaces the current page
+            console.log("Line 19");
             window.open("/client/user.html", "_self");
         }
     });
