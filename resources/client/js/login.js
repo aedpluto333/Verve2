d080fbad539e17eb653e51e826f66098dae183fd
Amy Page <71634574+aedpluto333@users.noreply.github.com>
2020-12-10 14:18:43 +0000

    update login.js to allow for cookies

--- resources/client/js/login.js
+++ resources/client/js/login.js
@@ -1,22 +1,20 @@
 // login function to allow user to access the website
 function logInUser() {
     console.log("Invoked logInUser()");
+    debugger;
     const formData = new FormData(document.getElementById('LoginForm'));
     let url = "/users/attemptlogin";
     fetch(url, {
         method: "POST",
         body: formData,
     }).then(response => {
-        console.log("Line 10");
         return response.json()
     }).then(response => {
-        console.log("Line 13");
         if (response.hasOwnProperty("Error")) {
-            console.log("Line 15");
             alert(JSON.stringify(response));
         } else {
             // URL replaces the current page
-            console.log("Line 19");
+            Cookies.set("SessionToken")
             window.open("/client/user.html", "_self");
         }
     });
