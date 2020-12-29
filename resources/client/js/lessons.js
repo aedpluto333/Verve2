e3a386227f359a16e9b2b332ccb39652398d58e1
aedpluto <amyedp123@outlook.com>
2020-12-29 14:15:14 +0000

    links courses page to appropriate lessons

--- resources/client/js/lessons.js
+++ resources/client/js/lessons.js
@@ -1,8 +1,8 @@
 "use strict";
 function getLessonsList() {
     //debugger;
-    console.log("Invoked getLessonsList();");     //console.log your BFF for debugging client side - also use debugger statement
-    const url = "/lessons/list/";    		// API method on web server will be in Users class, method list
+    console.log("Invoked getLessonsList();");
+    const url = "/lessons/list/";
     fetch(url, {
         method: "GET",				//Get method
     }).then(response => {
@@ -22,8 +22,25 @@ function formatLessonsList(myJSONArray){
         // inject div into the HTML page
         dataHTML += "<div class=\"lessonInfoBlock\">\n" +
             "        <p>"+item[1]+": "+item[2]+"</p>\n" +
-            "        <p><a href=\"index.html\">Start lesson >></a></p>\n" +
+            "        <p><a onClick=\"Cookies.set('LessonID', "+item[0]+");\" href=\"index.html\">Start lesson >></a></p>\n" +
             "    </div>";
     }
     document.getElementById("mainPanel").innerHTML = dataHTML;
+}
+
+function getLesson() {
+    //debugger;
+    console.log("Invoked getLesson();");     //console.log your BFF for debugging client side - also use debugger statement
+    const url = "/lessons/get/";    		// API method on web server will be in Users class, method list
+    fetch(url, {
+        method: "GET",				//Get method
+    }).then(response => {
+        return response.json();                 //return response as JSON
+    }).then(response => {
+        if (response.hasOwnProperty("Error")) { //checks if response from the web server has an "Error"
+            alert(JSON.stringify(response));    // if it does, convert JSON object to string and alert (pop up window)
+        } else {
+            formatLessonsList(response.Results);          //this function will create an HTML table of the data (as per previous lesson)
+        }
+    });
 }
